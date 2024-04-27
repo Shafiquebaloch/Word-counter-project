a12d1a8f28656655f008e0bd9d47624a37b42853
Shafiquebaloch <muhammadraheem4739733@gmail.com>
2024-04-28 00:28:36 +0500

    Pushing My project

---
+++ index.ts
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+import inquirer from "inquirer"
+
+ 
+console.log(" Welcome to  Word Counter Project")
+
+
+
+//Making a object
+let user_answer:{
+    sentence:string
+}=await inquirer.prompt([
+    
+    {
+        name:"sentence",
+        type:"input",
+        message:"Enter your sentence"
+    }
+]) 
+
+//Using .trim() & .split() functions
+let words = user_answer.sentence.trim().split(" ")
+console.log(words);
+//Using length method to count characters.
+let countwords=words.length;
+console.log(`There are ${countwords} words in your sentence`);
+
